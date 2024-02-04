e0bfbb4622902179038c59dc698138d0b67b3287
cabaktom <cabaktom@fit.cvut.cz>
2024-02-04 14:58:35 +0100

    Add products, closes #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   resources :lists
   resources :colors
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_04_112429) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_04_135055) do
   create_table "colors", force: :cascade do |t|
     t.string "name"
     t.string "hex_code"
@@ -30,6 +30,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_04_112429) do
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "barcode"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -45,4 +54,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_04_112429) do
   add_foreign_key "colors", "users"
   add_foreign_key "lists", "colors"
   add_foreign_key "lists", "users"
+  add_foreign_key "products", "users"
 end
