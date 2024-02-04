e2e96a3ee3153b0ff3c2b6544528fc2932cb4f48
cabaktom <cabaktom@fit.cvut.cz>
2024-02-04 22:10:49 +0100

    Add username and change login option to username or email, closes #17

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,23 @@ class User < ApplicationRecord
   has_many :products, dependent: :destroy
   has_many :items, dependent: :destroy
   has_many :notifications, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true # prevent using email as username
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      conditions[:email].downcase! if conditions[:email]
+      where(conditions.to_h).first
+    end
+  end
 end
