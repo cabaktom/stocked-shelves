61a7666f040afc23be0b968ba3ef846a5ad7ecf6
cabaktom <cabaktom@fit.cvut.cz>
2024-04-05 20:48:51 +0200

    Add API ListsController and JB views

--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,7 @@ Rails.application.routes.draw do
         delete 'image', to: 'products#delete_image_attachment', on: :member
       end
       resources :items, only: %i[index show create update destroy]
+      resources :lists, only: %i[index show create update destroy]
 
       # Other
       get 'current_user', to: 'current_user#show', as: :current_user
