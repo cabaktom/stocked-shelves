ab6289a6cdb08cfde5a4026188802820c061e8f2
cabaktom <cabaktom@fit.cvut.cz>
2024-04-05 19:02:19 +0200

    Refactor product controller and JB view with HATEOAS links

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,7 +4,6 @@ module Api
   module V1
     class ProductsController < ApiController
       before_action :set_product, only: %i[show update destroy delete_image_attachment]
-      before_action :set_view_path, only: %i[create update]
 
       # GET /api/v1/products
       def index
@@ -20,32 +19,31 @@ module Api
         @product.user_id = current_user.id
 
         if @product.save
-          render 'show', status: :created
+          render :show, status: :created
         else
-          render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+          render json: { error: @product.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
       # PATCH/PUT /api/v1/products/1
       def update
         if @product.update(product_params)
-          render 'show', status: :ok
+          render :show, status: :ok
         else
-          render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+          render json: { error: @product.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
       # DELETE /api/v1/products/1
       def destroy
         @product.destroy!
-
         head :no_content
       end
 
       # DELETE /api/v1/products/1/image
       def delete_image_attachment
         @image = ActiveStorage::Blob.find_signed(params[:signed_id])
-        return render json: { errors: 'Image not found.' }, status: :not_found if @image.nil?
+        return render json: { error: 'Image not found.' }, status: :not_found if @image.nil?
 
         @image.attachments.first.purge
         render status: :no_content
@@ -56,16 +54,12 @@ module Api
       # Use callbacks to share common setup or constraints between actions.
       def set_product
         @product = current_user.products.find_by_id(params[:id])
-        render json: { errors: 'Product not found.' }, status: :not_found if @product.nil?
+        render json: { error: 'Product not found.' }, status: :not_found if @product.nil?
       end
 
       # Only allow a list of trusted parameters through.
       def product_params
         params.require(:product).permit(:name, :barcode, :image)
-      end
-
-      def set_view_path
-        prepend_view_path 'app/views/api/v1/products'
       end
     end
   end
